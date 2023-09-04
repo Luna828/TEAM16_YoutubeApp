410c9e2570e824707ef18275f14cc0ab2ee2b88d
Luna828 <eskw0701@naver.com>
2023-09-04 19:25:30 +0900

    [FEAT] loginViewController와 연결

--- YoutubeCloneApp/LoginPage/LoginPage.swift
+++ YoutubeCloneApp/LoginPage/LoginPage.swift
@@ -9,4 +9,28 @@ import UIKit
 
 class LoginPageViewController: UIViewController {
     
+    var email = String()
+    var password = String()
+    
+    @IBAction func emailTextField(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.email = text
+    }
+    
+    @IBAction func passwordTextField(_ sender: UITextField) {
+        let text = sender.text ?? ""
+        self.password = text
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 }

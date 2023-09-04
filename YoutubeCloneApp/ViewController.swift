6f5d41d68f0d1daa8c7029b0fb22bb1593047f2c
Luna828 <eskw0701@naver.com>
2023-09-04 10:52:17 +0900

    [FEAT]: commit init

---
+++ YoutubeCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeCloneApp
+//
+//  Created by t2023-m0050 on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

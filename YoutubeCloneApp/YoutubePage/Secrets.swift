e5607f89fc80e25b24d07c0f0ed96cea369ef2a1
Leehan-sol <ho20128@naver.com>
2023-09-07 10:22:23 +0900

    [FEAT] Secret key 저장

---
+++ YoutubeCloneApp/YoutubePage/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  YoutubeCloneApp
+//
+//  Created by t2023-m0050 on 2023/09/06.
+//
+
+import Foundation
+
+//APiKey
+let LUNA_KEY = "AIzaSyCtToVVWohC67KJRTPMPNIAYAEoqz3s7c8"
+

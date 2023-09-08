f83ad729ab33e9f3529d8d898d57a872726ee44d
DearYesang <scyea@korea.ac.kr>
2023-09-08 19:58:46 +0900

    [FEAT]  은경님 코드를 fork하여 MainPage의 영상 클릭 시 DetailPage로 넘어가도록 구현함

--- YoutubeCloneApp/MainPage/MainPageViewController.swift
+++ YoutubeCloneApp/MainPage/MainPageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class MainPageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+final class MainPageViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
     
     private let headerView: MainPageHeaderView = .init(frame: .zero)
     private var thumbnailList: Thumbnails?
@@ -95,5 +95,26 @@ final class MainPageViewController: UIViewController, UICollectionViewDelegate, 
         return 8
     }
 }
+
+extension MainPageViewController: UICollectionViewDataSource {
+    //cell을 클릭했을 때 작동함수
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        //url
+        let selectedVideo = thumbnailList?.items[indexPath.row].snippet.thumbnails.high.url //"https://i.ytimg.com/vi/cRe9KMvYfWU/mqdefault.jpg"
+        
+        
+        if let videoId = selectedVideo?.getVideoID() {
+            let detailPage = DetailPageViewController(videoId: videoId)
+            //화면전환 코드 (YOUTUBEPage -> DetailPage)
+            //stack view (push, pop)
+            navigationController?.pushViewController(detailPage, animated: true)
+        } else {
+            print("Video가 없습니다")
+        }
+        
+    }
+}
+
 
 
